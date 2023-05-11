c6e2cbcbf9260a3e4ce7ee8008c39d218aa90bed
Sarah Groves <sarahmaddoxgroves@gmail.com>
2023-05-11 15:06:37 -0500

    validation plots by datasets (external validation)

--- network-inference-DIRECT-NET/external_validation_summary.py
+++ network-inference-DIRECT-NET/external_validation_summary.py
@@ -15,93 +15,57 @@ import matplotlib.gridspec as gridspec
 mpl.rcParams['figure.max_open_warning'] = 0
 mpl.rcParams['font.family'] = 'sans-serif'
 mpl.rcParams['font.sans-serif'] = ['Arial']
-mpl.rcParams['font.size'] = 16
+# mpl.rcParams['font.size'] = 16
 mpl.rcParams['lines.linewidth'] = 1.25
 cmap = mpl.rcParams['axes.prop_cycle'].by_key()['color']
 
 # Set paths
 dir_prefix = '/Users/smgroves/Documents/GitHub/multiome-analysis/network-inference-DIRECT-NET'
 network_path = 'networks/DIRECT-NET_network_with_FIGR_threshold_0_no_NEUROG2_top8regs_NO_sinks_NOCD24_expanded.csv'
-
 brcd = str(9999)
-validation_fname = "validation"
-VAL_DIR = f"{dir_prefix}/{brcd}/{validation_fname}"
 
-subfolders = [f.path for f in os.scandir(VAL_DIR) if f.is_dir()]
-print(subfolders)
+def get_summary_stats(VAL_DIR, dir_prefix, network_path, remove_sources = True):
+    subfolders = [f.path for f in os.scandir(VAL_DIR) if f.is_dir()]
+    print(subfolders)
 
-stats_df = pd.DataFrame(columns=["dataset", "gene", "accuracy", "balanced_accuracy_score", "f1", "roc_auc_score",
-                                 "precision", "recall", "explained_variance", "max_error", "r2", "log-loss"])
-for f in subfolders:
-    try:
-        if f.split("-")[-1] == 'validation':
-            tmp = pd.read_csv(f"{f}/summary_stats.csv")
-            tmp['dataset'] = f.split("/")[-1].split("-")[0]
-            stats_df = pd.concat([stats_df, tmp])
-    except FileNotFoundError: pass
+    stats_df = pd.DataFrame(columns=["dataset", "gene", "accuracy", "balanced_accuracy_score", "f1", "roc_auc_score",
+                                     "precision", "recall", "explained_variance", "max_error", "r2", "log-loss"])
+    for f in subfolders:
+        try:
+            if f.split("-")[-1] == 'validation':
+                tmp = pd.read_csv(f"{f}/summary_stats.csv")
+                tmp['dataset'] = f.split("/")[-1].split("-")[0]
+                stats_df = pd.concat([stats_df, tmp])
+        except FileNotFoundError: pass
 
-stats_df = stats_df.drop('Unnamed: 0', axis = 1)
+    stats_df = stats_df.drop('Unnamed: 0', axis = 1)
 
-graph, vertex_dict = bb.load.load_network(f'{dir_prefix}/{network_path}', remove_sinks=False, remove_selfloops=False,
-                                              remove_sources=False)
+    graph, vertex_dict = bb.load.load_network(f'{dir_prefix}/{network_path}', remove_sinks=False, remove_selfloops=False,
+                                                  remove_sources=False)
 
-v_names, nodes = bb.utils.get_nodes(vertex_dict, graph)
+    v_names, nodes = bb.utils.get_nodes(vertex_dict, graph)
 
-sources = []
-sinks = []
-for i in range(len(nodes)):
-    if graph.vp.source[i] == 1: sources.append(graph.vp.name[i])
-    if graph.vp.sink[i] == 1: sinks.append(graph.vp.name[i])
-print("Sources: ", len(sources), sources)
-print("Sinks: ",len(sinks), sinks)
+    sources = []
+    sinks = []
+    for i in range(len(nodes)):
+        if graph.vp.source[i] == 1: sources.append(graph.vp.name[i])
+        if graph.vp.sink[i] == 1: sinks.append(graph.vp.name[i])
+    print("Sources: ", len(sources), sources)
+    print("Sinks: ",len(sinks), sinks)
 
-stats_df['source'] = [True if x in sources else False for x in stats_df['gene']]
+    stats_df['source'] = [True if x in sources else False for x in stats_df['gene']]
 
-print(stats_df.head())
+    print(stats_df.head())
 
-# Remove sources, which will be artificially high
-stats_df = stats_df.loc[stats_df['source']== False]
-#
-# pdf = PdfPages(f'{VAL_DIR}/external_validation_summary.pdf')
-#
-# for stat in ["accuracy", "balanced_accuracy_score", "f1", "roc_auc_score",
-#              "precision", "recall", "explained_variance", "max_error", "r2", "log-loss"]:
-#     figure = plt.figure(figsize = (5,len(stats_df.dataset.unique())))
-#     gs = gridspec.GridSpec(1, 1)
-#     ax = plt.subplot(gs[0])
-#     sns.boxplot(data = stats_df, y = 'dataset', x = stat, order = sorted(stats_df.dataset.unique()),
-#                 ax = ax)
-#     sns.stripplot(data = stats_df, y = 'dataset', x = stat, alpha = .5,
-#                   order = sorted(stats_df.dataset.unique()), ax = ax, color=".2")
-#     plt.tight_layout()
-#     pdf.savefig()
-#     plt.clf()
-#
-# pdf.close()
-#
-#
-# pdf = PdfPages(f'{VAL_DIR}/external_validation_summary_by_gene.pdf')
-#
-# for stat in ["accuracy", "balanced_accuracy_score", "f1", "roc_auc_score",
-#              "precision", "recall", "explained_variance", "max_error", "r2", "log-loss"]:
-#     figure = plt.figure(figsize = (5,len(stats_df.gene.unique())))
-#     gs = gridspec.GridSpec(1, 1)
-#     ax = plt.subplot(gs[0])
-#     sns.boxplot(data = stats_df, y = 'gene', x = stat, order = sorted(stats_df.gene.unique()),
-#                 ax = ax, color = 'white')
-#     sns.swarmplot(data = stats_df, y = 'gene', x = stat, alpha = .5, hue = 'dataset',
-#                   order = sorted(stats_df.gene.unique()), ax = ax, )
-#     plt.tight_layout()
-#     pdf.savefig()
-#     plt.clf()
-#
-#
-# pdf.close()
-
+    # Remove sources, which will be artificially high
+    if remove_sources:
+        stats_df = stats_df.loc[stats_df['source']== False]
+    return stats_df
 
 def plot_summary_boxplots(stats_df, VAL_DIR, file_name, groupby = 'dataset', hue = None, dots = 'swarm',height = 1.0,
                           stats = ["accuracy", "balanced_accuracy_score", "f1", "roc_auc_score","precision",
-                                   "recall", "explained_variance", "max_error", "r2", "log-loss"]):
+                                   "recall", "explained_variance", "max_error", "r2", "log-loss"],
+                          ordered_by = "alphabetical"):
     pdf = PdfPages(f'{VAL_DIR}/{file_name}')
 
     for stat in stats:
@@ -109,19 +73,41 @@ def plot_summary_boxplots(stats_df, VAL_DIR, file_name, groupby = 'dataset', hue
         figure = plt.figure(figsize=(5, height*len(stats_df[groupby].unique())))
         gs = gridspec.GridSpec(1, 1)
         ax = plt.subplot(gs[0])
-        sns.boxplot(data=stats_df, y=groupby, x=stat, order=sorted(stats_df[groupby].unique()),
-                    ax=ax, color='white')
+        if ordered_by == 'alphabetical':
+            order = sorted(stats_df[groupby].unique())
+        elif ordered_by == 'mean':
+            order = stats_df.groupby(groupby).mean().sort_values(by = stat, ascending=False).index
+        elif ordered_by == 'median':
+            order = stats_df.groupby(groupby).median().sort_values(by = stat, ascending = False).index
+        PROPS = {'boxprops': {'facecolor': 'none', 'edgecolor': 'black'}}
+        sns.boxplot(data=stats_df, y=groupby, x=stat, order=order,
+                    ax=ax, color='white', **PROPS)
         if dots == 'swarm':
             sns.swarmplot(data=stats_df, y=groupby, x=stat, hue=hue,
-                      order=sorted(stats_df[groupby].unique()), ax=ax, )
+                      order=order, ax=ax, size = 3)
         elif dots == 'strip':
             sns.stripplot(data=stats_df, y='dataset', x=stat, alpha=.5,
-                          order=sorted(stats_df.dataset.unique()), ax=ax, color=".2")
+                          order=order, ax=ax, color=".2")
+        if stat in ["explained_variance", "r2"]:
+            plt.axvline(x = 0, linestyle = '--', color = 'lightgray')
+        plt.setp(ax.lines, color=".1")
+        plt.xticks(size = 16)
+        plt.yticks(size = 16)
+        plt.xlabel(stat.capitalize(), size = 20)
+        plt.ylabel(groupby.capitalize(), size = 20)
         plt.tight_layout()
         pdf.savefig()
         plt.clf()
 
     pdf.close()
 
-plot_summary_boxplots(stats_df, VAL_DIR, file_name='external_validation_summary_by_gene.pdf',groupby='gene',
-                      hue = 'dataset',dots = 'swarm', height = .5, stats = ['accuracy'])
+## Plots for allograft validation datasets
+validation_folder = "validation/allograft_validation"
+VAL_DIR = f"{dir_prefix}/{brcd}/{validation_folder}"
+
+stats_df = get_summary_stats(VAL_DIR, dir_prefix, network_path)
+# plot_summary_boxplots(stats_df, VAL_DIR, file_name='external_validation_summary_mean.pdf',groupby='dataset',
+#                       hue = None,dots = 'strip', height = 1, ordered_by='mean')
+
+plot_summary_boxplots(stats_df, VAL_DIR, file_name='external_validation_summary_mean_by_gene.pdf',groupby='gene',
+                      hue = 'dataset',dots = 'swarm', height = .5, ordered_by='mean')
--- network-inference-DIRECT-NET/preprocess_adata.py
+++ network-inference-DIRECT-NET/preprocess_adata.py
@@ -7,22 +7,24 @@ import numpy as np
 
 DIRECT_NET_INDIR = "./DIRECT-NET-FILES/"
 
-def preprocess_adata(adata, DIRECT_NET_INDIR, outfile_name = "adata_imputed.csv", extra_genes = None, imputed_layer = 'imputed',
-                     species = 'mouse'):
-    direct_net = pd.read_csv(os.path.join(DIRECT_NET_INDIR,"Direct_net.csv"), header = 0, index_col = 0)
+
+def preprocess_adata(adata, DIRECT_NET_INDIR, outfile_name="adata_imputed.csv", extra_genes=None,
+                     imputed_layer='imputed',
+                     species='mouse'):
+    direct_net = pd.read_csv(os.path.join(DIRECT_NET_INDIR, "Direct_net.csv"), header=0, index_col=0)
     direct_net['Target_gene'] = [i.upper() for i in direct_net['Target_gene']]
     # "TF motif" column is parent node, "Target gene" is child node
     tfs = []
-    for i,r in direct_net.iterrows():
+    for i, r in direct_net.iterrows():
         tfs.append(r['TF motif'])
         tfs.append(r["Target_gene"])
     tfs = list(set(tfs))
 
-    figr = pd.read_csv(os.path.join(DIRECT_NET_INDIR, "FigR_DORC_TF.csv"), header = 0, index_col=0)
+    figr = pd.read_csv(os.path.join(DIRECT_NET_INDIR, "FigR_DORC_TF.csv"), header=0, index_col=0)
     figr.DORC = [i.upper() for i in figr.DORC]
     figr.Motif = [i.upper() for i in figr.Motif]
 
-    for i,r in figr.iterrows():
+    for i, r in figr.iterrows():
         tfs.append(r['Motif'])
         tfs.append(r["DORC"])
 
@@ -39,15 +41,17 @@ def preprocess_adata(adata, DIRECT_NET_INDIR, outfile_name = "adata_imputed.csv"
     print(list(set(tfs).difference(set([i.upper() for i in adata.var_names]))))
 
     if species == 'mouse':
-        adata_net = adata[:,[i.capitalize() for i in overlap]]
+        adata_net = adata[:, [i.capitalize() for i in overlap]]
     elif species == 'human':
-        adata_net = adata[:,[i.upper() for i in overlap]]
+        adata_net = adata[:, [i.upper() for i in overlap]]
     else:
         print("Species must be mouse or human.")
     print(adata_net)
-    print(adata_net.layers[imputed_layer][0:10,0:10])
-    adata_imputed = pd.DataFrame(adata_net.layers[imputed_layer], index=adata_net.obs_names, columns=adata_net.var_names)
+    print(adata_net.layers[imputed_layer][0:10, 0:10])
+    adata_imputed = pd.DataFrame(adata_net.layers[imputed_layer], index=adata_net.obs_names,
+                                 columns=adata_net.var_names)
     adata_imputed.to_csv(f"./data/{outfile_name}")
+
 
 # adata = cr.read('../data/M2/adata_04_nodub.h5ad')
 # preprocess_adata(adata, DIRECT_NET_INDIR, outfile_name = "adata_04_nodubs_imputed_M2.csv")
@@ -66,6 +70,13 @@ def preprocess_adata(adata, DIRECT_NET_INDIR, outfile_name = "adata_imputed.csv"
 #     adata = cr.read(f"../data/external_validation_looms/{allo}_allograftdata.h5ad")
 #     preprocess_adata(adata, DIRECT_NET_INDIR, outfile_name = f"adata_{allo}_allografts.csv", extra_genes=['CD24', 'CD44', 'EPCAM', 'ICAM1', 'NCAM1'])
 
-adata = cr.read(f"../data/external_validation_looms/adata.SCLC.010920.h5ad")
-preprocess_adata(adata, DIRECT_NET_INDIR, outfile_name= f"adata_human_tumors_MSK.csv", extra_genes=['CD24', 'CD44', 'EPCAM', 'ICAM1', 'NCAM1'],
-                 imputed_layer="imputed_normalized", species = 'human')
+# adata = cr.read(f"../data/external_validation_looms/adata.SCLC.010920.h5ad")
+# preprocess_adata(adata, DIRECT_NET_INDIR, outfile_name= f"adata_human_tumors_MSK.csv", extra_genes=['CD24', 'CD44', 'EPCAM', 'ICAM1', 'NCAM1'],
+#                  imputed_layer="imputed_normalized", species = 'human')
+
+for tumor in ["PleuralEffusion", "RU426", "RU779", "RU1065", "RU1066", "RU1080", "RU1108", "RU1124", "RU1144", "RU1145",
+              "RU1152", "RU1181", "RU1195", "RU1215", "RU1229", "RU1231", "RU1293", "RU1311", "RU1322"]:
+    adata = cr.read(f"../data/external_validation_looms/human_tumors/{tumor}_human_tumor_data.h5ad")
+    preprocess_adata(adata, DIRECT_NET_INDIR, outfile_name=f"human_tumors/adata_{tumor}.csv",
+                     extra_genes=['CD24', 'CD44', 'EPCAM', 'ICAM1', 'NCAM1'], imputed_layer="imputed_normalized",
+                     species = 'human')
