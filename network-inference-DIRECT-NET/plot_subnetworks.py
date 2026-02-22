862d5b1735f563dd7c3dff4bb150e5a235f35b2a
smgroves <sarahmaddoxgroves@gmail.com>
2026-02-22 11:08:34 -0500

    6667 subnetwortks

--- network-inference-DIRECT-NET/plot_subnetworks.py
+++ network-inference-DIRECT-NET/plot_subnetworks.py
@@ -9,12 +9,12 @@ import matplotlib.pyplot as plt
 from bb_utils import draw_grn
 
 dir_prefix = '/Users/smgroves/Documents/GitHub/multiome-analysis/network-inference-DIRECT-NET'
-brcd = str(1112)
+brcd = str(6667)
 save_dir = f"{dir_prefix}/{brcd}"
 
 edge_weights = pd.read_csv(f"{dir_prefix}/{brcd}/rules/edge_weights.csv", header = 0, index_col=0)
 # network_file = f"{dir_prefix}/networks/DIRECT-NET_network_with_FIGR_threshold_0_no_NEUROG2_top8regs_NO_sinks_NOCD24_expanded.csv"
-network_file = f"{dir_prefix}/networks/DIRECT-NET_network_2020db_0.1_top8regs_wo_sinks.csv"
+network_file = f"{dir_prefix}/networks/feature_selection/DIRECT-NET_network_2020db_0.1/combined_DIRECT-NET_network_2020db_0.1_Lasso_wo_sinks_RORA_RORB_combined.csv"
 nodes = edge_weights.index
 
 def plot_subgraph(keep_nodes, network_file, nodes, edge_weights, keep_parents = True, keep_children = True,
@@ -89,7 +89,7 @@ def plot_subgraph(keep_nodes, network_file, nodes, edge_weights, keep_parents = 
 # ASCL1, RORB, NFIB, EGR1,REST and TCF7L2.
 
 # for g in ['ASCL1', 'RORB', 'NFIB', 'EGR1', 'REST', 'TCF7L2']:
-for g in ['TEAD1','RBPJ']:
+for g in ['ASCL1','RORA_RORB']:
     plot_subgraph([g], network_file, nodes, edge_weights, keep_parents = True, keep_children = True,
               save_dir = f"{dir_prefix}/{brcd}", arrows = "curved", show = False, save = True, off_node_arrows_gray=True,
               weight = 3)
